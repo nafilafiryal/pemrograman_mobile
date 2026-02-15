50b4dc50502a7b48a392a92e7ddd215a30bc11ec
nafilafiryal <nafilahfiryal05@gmail.com>
2026-02-15 20:19:34 +0700

    delete

--- dart_basic1/bin/dart_basic1.dart
+++
@@ -1,5 +0,0 @@
-import 'package:dart_basic1/dart_basic1.dart' as dart_basic1;
-
-void main(List<String> arguments) {
-  print('Hello world: ${dart_basic1.calculate()}!');
-}
--- dart_basic1/test/dart_basic1_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dart_basic1/dart_basic1.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
