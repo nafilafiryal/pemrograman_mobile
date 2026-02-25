a56d0a36871c46d0dd9d48ee91840b1bdbb48cd3
nafilafiryal <nafilahfiryal05@gmail.com>
2026-02-25 15:30:50 +0700

    feat: add kalkulator aritmatika dasar (studi kasus 2)

---
+++ week2/src/dart_study_case2/bin/dart_study_case2.dart
@@ -0,0 +1,18 @@
+import 'package:dart_study_case2/dart_study_case2.dart' as dart_study_case2;
+
+void main() {
+  int angka1 = 10;
+  int angka2 = 5;
+
+  int penjumlahan = angka1 + angka2;
+  int pengurangan = angka1 - angka2;
+  int perkalian = angka1 * angka2;
+  double pembagian = angka1 / angka2;
+  int modulo = angka1 % angka2;
+
+  print('$angka1 + $angka2 = $penjumlahan');
+  print('$angka1 - $angka2 = $pengurangan');
+  print('$angka1 * $angka2 = $perkalian');
+  print('$angka1 / $angka2 = $pembagian');
+  print('$angka1 % $angka2 = $modulo');
+}
